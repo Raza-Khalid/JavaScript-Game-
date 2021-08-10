b8081241d5db43aa4c257e96b94529b2a4791c2b
Raza Khalid <razakhalid69@gmail.com>
2021-08-10 14:10:44 -0400

    game jump logic done

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 //variable for  character
 // variable for block
-const among = document.getElementsByClassName('.among')
-const block = document.getElementsByClassName('.block')
-
+const among = document.querySelector('.among')
+const block = document.getElementsByClassName('block')
 
 // fucntion for jump
-function jump(){
+//when upper key pressed change the top to 300px and 1 sec later
+// bring the player back down to 320px
 
+function jump() {
+  among.style.top = `210px`
+
+  function down() {
+    among.style.bottom = `330px`
+  }
+  // adding a timeOut method for the character, sets the duration for the character
+  setTimeout(() => {
+    among.style.top = `330px`
+  }, 500)
 }
 
-// addEventListener for 
-// onclick or UP keys  
-// function control(e) {
-//   if (e.keycode === 38)
-// }
+// addEventListener for for pressing the key
+// keydown means the key is pressed
+// number 38 is the number for the upper key
+window.addEventListener('keydown', handleKeyPress)
+function handleKeyPress(e) {
+  if (e.keyCode === 38) {
+    jump()
+  }
+}
+
+//keyup means the key is NOT pressed
+window.addEventListener('keyup', handleRelease)
+function handleRelease(e) {
+  if (e.keyCode === 38) {
+    down()
+  }
+}
