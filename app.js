ff4749b7414fb0a781af2eade3b0a1f140c53a9e
Raza Khalid <razakhalid69@gmail.com>
2021-08-11 23:52:45 -0400

    added more functionality

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-//variable for  character
-// variable for block
 const among = document.querySelector('.among')
-const block = document.getElementsByClassName('block')
+const blocks = [...document.getElementsByClassName('block')]
+// const buttons = [...document.querySelector('button')]
+let interval = 2
+let running = true
+
+function start() {
+  blocks.forEach((block) => {
+    block.style.animation = 'block ' + interval + 's infinite linear'
+    interval = interval + 1
+  })
+  // buttons.forEach(buttons => {
+
+  // });
+}
+
+function end() {
+  blocks.forEach((block) => {
+    block.style.animation = ''
+  })
+}
 
 // fucntion for jump
 //when upper key pressed change the top to 300px and 1 sec later
@@ -10,13 +27,13 @@ const block = document.getElementsByClassName('block')
 function jump() {
   among.style.top = `230px`
 
-  function down() {
-    among.style.top = `330px`
-  }
   // adding a timeOut method for the character, sets the duration of the jump for the character
   setTimeout(() => {
     among.style.top = `330px`
   }, 300)
+}
+function down() {
+  among.style.top = `330px`
 }
 
 // function to see if the block and the character collided
@@ -25,15 +42,18 @@ let checkCollision = setInterval(function () {
   let amongTop = parseInt(
     window.getComputedStyle(among).getPropertyValue('top')
   )
-
   // getting blocks current position
-  let blockLeft = parseInt(
-    window.getComputedStyle(block).getPropertyValue('left')
-  )
-
-  if (blockLeft < 50 && blockLeft > 0 && amongTop >= 330) {
-    alert('Game Over')
-  }
+  blocks.forEach((block) => {
+    let blockLeft = parseInt(
+      window.getComputedStyle(block).getPropertyValue('left')
+    )
+    if (blockLeft < 40 && blockLeft > 0 && amongTop >= 330) {
+      console.log(blockLeft, amongTop)
+      running = false
+      end()
+      // alert('Game Over')
+    }
+  })
 }, 10)
 
 // addEventListener for pressing the key
@@ -46,10 +66,10 @@ function handleKeyPress(e) {
   }
 }
 
-//keyUP means the key is NOT pressed
-window.addEventListener('keyup', handleRelease)
-function handleRelease(e) {
-  if (e.keyCode === 38) {
-    down()
-  }
-}
+// //keyUP means the key is NOT pressed
+// window.addEventListener('keyup', handleRelease)
+// function handleRelease(e) {
+//   if (e.keyCode === 38) {
+//     down()
+//   }
+// }
